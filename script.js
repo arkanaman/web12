c87305ab5040bbf6fa834e6a4c9a254ddbb9fb4f
arkanaman <arkanaman@mail.ru>
2019-03-14 19:50:54 +0200

    1

--- script.js
+++ script.js
@@ -1,53 +1,55 @@
 // 1) Array.prototype.every
 
 var arrayEvery = [8, 54, 18, 130, 44];
-var checkEvery = 0;
 
 var resultEvery = arrayEvery.every(function (element) {
     return typeof element === "number";
 });
 console.log('Метод "every" - ' + resultEvery);
 
-function every() {
-    for (var i = 0; i < arrayEvery.length; i++) {
-        if (typeof arrayEvery[i] === "number") {
-            checkEvery++;
-            if (checkEvery == arrayEvery.length) {
+Array.prototype.myEvery = function (array) {
+    var check = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (typeof array[i] === "number") {
+            check++;
+            if (check == array.length) {
                 return true;
             }
         } else {
             return false;
         }
     }
-}
-console.log('Полифил метода "every" - ' + every());
+};
+
+console.log('Полифил метода "every" - ' + Array.prototype.myEvery(arrayEvery));
 console.log('');
 
 
 // 2) Array.prototype.some
 
-var arraySome = ['8', '54', 18, '130', '44'];
-var checkSome = 0;
+var arraySome = ['8', '54', '18', '130', '44'];
 
 var resultSome = arraySome.some(function (element) {
     return typeof element === "number";
 });
 console.log('Метод "some" - ' + resultSome);
 
-function some() {
-    for (var i = 0; i < arraySome.length; i++) {
-        if (typeof arraySome[i] === "number") {
+Array.prototype.mySome = function (array) {
+    var check = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (typeof array[i] === "number") {
             return true;            
         } else {
-            checkSome++;
-            if (checkSome == arraySome.length) {
+            check++;
+            if (check == array.length) {
                 return false;
             }
         }
     }
-}
+};
 
-console.log('Полифил метода "some" - ' + some());
+
+console.log('Полифил метода "some" - ' + Array.prototype.mySome(arraySome));
 console.log('');
 
 
@@ -60,15 +62,15 @@ var resultMap = arrayMap.map(function (element) {
 });
 console.log('Метод "map" - ' + resultMap);
 
-function map() {
-    var PollMap = [];
-    for (var i = 0; i < arrayMap.length; i++) {
-        PollMap.push(arrayMap[i] * 2);
+Array.prototype.myMap = function (array) {
+    var pull = [];
+    for (var i = 0; i < array.length; i++) {
+        pull.push(array[i] * 2);
     }
-    return PollMap;
+    return pull;
 }
 
-console.log('Полифил метода "map" - ' + map());
+console.log('Полифил метода "map" - ' + Array.prototype.myMap(arrayMap));
 console.log('');
 
 
@@ -82,39 +84,39 @@ arrayForEach.forEach(function(element) {
 });
 
 console.log('Полифил метода "forEach" - ');
-function forEach() {
-    for (var i = 0; i < arrayForEach.length; i++) {
-        console.log(arrayForEach[i]);
+Array.prototype.myForEach = function (array) {
+    for (var i = 0; i < array.length; i++) {
+        console.log(array[i]);
     }
 }
-forEach();
+Array.prototype.myForEach(arrayForEach);
 console.log('');
 
 
 // 5) Array.prototype.find
 
 var arrayFind = [8, 54, 18, 'строка', 44, 'строка2'];
-var checkFind = 0;
 
 var resultFind = arrayFind.find(function (element) {
     return typeof element === 'string';
 });
 console.log('Метод "find" - ' + resultFind);
 
-function find() {
-    for (var i = 0; i < arrayFind.length; i++) {
-        if (typeof arrayFind[i] === 'string') {
-            return arrayFind[i];
+Array.prototype.myFind = function (array) {
+    var check = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (typeof array[i] === 'string') {
+            return array[i];
         } else {
-            checkFind++;
-            if (checkFind == arrayFind.length) {
+            check++;
+            if (check == array.length) {
                 return undefined;
             }
         }
     }
 }
 
-console.log('Полифил метода "find" - ' + find());
+console.log('Полифил метода "find" - ' + Array.prototype.myFind(arrayFind));
 console.log('');
 
 
@@ -122,21 +124,21 @@ console.log('');
 
 var arrayFilter = [8, 54, 18, 'строка', 44, 'строка2', 'строка 3'];
 
-const resultFilter = arrayFilter.filter(string => typeof string === 'string');
+var resultFilter = arrayFilter.filter(string => typeof string === 'string');
 
 console.log('Метод "filter" - ' + resultFilter);
 
-function filter() {
-    var PollFilter = [];
-    for (var i = 0; i < arrayFilter.length; i++) {
-        if (typeof arrayFilter[i] === 'string') {
-            PollFilter.push(arrayFilter[i]);
+Array.prototype.myFilter = function (array) {
+    var pull = [];
+    for (var i = 0; i < array.length; i++) {
+        if (typeof array[i] === 'string') {
+            pull.push(array[i]);
         }        
     }
-    return PollFilter;
+    return pull;
 }
 
-console.log('Полифил метода "filter" - ' + filter());
+console.log('Полифил метода "filter" - ' + Array.prototype.myFilter(arrayFilter));
 console.log('');
 
 
@@ -147,17 +149,18 @@ var arrayResult = [];
 console.log('Метод "flat" - ');
 console.log(arrayFlat.flat(1));
 
-function flat(array, depth) {
+Array.prototype.myFlat = function (array, depth) {
     for (var i = 0; i < array.length; i++) {
         if (Array.isArray(array[i]) && depth != 0) {
             depth--;
-            flat(array[i], depth);
+            Array.prototype.myFlat(array[i], depth);
         } else {
             arrayResult.push(array[i]);
         }
     }
 }
-flat(arrayFlat, 1);
+
+Array.prototype.myFlat(arrayFlat, 1);
 
 console.log('Полифил метода "flat" - ');
 console.log(arrayResult);
@@ -170,7 +173,7 @@ var arrayReduce = [1, 2, 3, 4, 5];
 
 console.log('Метод "reduce" - ' + arrayReduce.reduce((accumulator, currentValue) => accumulator + currentValue, 5));
 
-function reduce(array, startValue) {
+Array.prototype.myReduce = function (array, startValue) {
     var accumulator = 0;
     if (startValue >= 0) {
         accumulator += startValue; 
@@ -180,5 +183,4 @@ function reduce(array, startValue) {
     }
     return accumulator;
 }
-console.log('Полифил метода "reduce" - ' + reduce(arrayReduce, 5));
-
+console.log('Полифил метода "reduce" - ' + Array.prototype.myReduce(arrayReduce, 5));
